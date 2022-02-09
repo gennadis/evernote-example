3e6c7ac3189a88c0206dab901d4011812a6eec78
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-09 15:03:25 +0300

    Switch `dump_inbox.py` to dotenv usage

--- dump_inbox.py
+++ dump_inbox.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python 
 import argparse
+import os
 
 from evernote.api.client import EvernoteClient
 from evernote.api.client import NoteStore
 from bs4 import BeautifulSoup
-
-from config import Settings
+from dotenv import load_dotenv
 
     
 def get_notebook_list(note_store, notebook_guid, number=10, offset=0):
@@ -37,15 +37,17 @@ if __name__ == '__main__':
                         help='number of records to dump')
     args = parser.parse_args()
 
-    config = Settings()
+    load_dotenv()
+    evernote_personal_token = os.getenv('EVERNOTE_PERSONAL_TOKEN')
+    inbox_notebook_guid = os.getenv('INBOX_NOTEBOOK_GUID')
 
     client = EvernoteClient(
-        token=config.EVERNOTE_PERSONAL_TOKEN,
+        token=evernote_personal_token,
         sandbox=False
     )
     note_store = client.get_note_store()
 
-    notes = get_notebook_list(note_store, config.INBOX_NOTEBOOK_GUID, args.number).notes
+    notes = get_notebook_list(note_store, inbox_notebook_guid, args.number).notes
 
     # print('Notes', notes)
     
